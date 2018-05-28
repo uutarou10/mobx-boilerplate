68998d62cb15078368f417373d872446ce7b5ae1
Kota Nonaka <kota.kaicho@gmail.com>
2018-05-28 23:42:10 +0900

    It's works!

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
             ],
             plugins: [
               ['@babel/plugin-proposal-decorators', { legacy: true }],
+              ['@babel/plugin-proposal-class-properties', { loose: true }],
               'react-hot-loader/babel'
             ]
 
